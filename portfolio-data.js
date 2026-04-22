e5f89941be2f6d86575fed0bd42b05a0d014f0de
Fajar Purwadi <101885122+980829@users.noreply.github.com>
2026-04-22 08:14:25 +0700

    Add files via upload

--- portfolio-data.js
+++ portfolio-data.js
@@ -2,55 +2,59 @@
 // DATA PORTFOLIO — edit bagian ini untuk update data saham
 // ============================================================
 const PORTFOLIO_DATA = {
-  owner: "Fajar",
-  lastUpdate: "22 Apr 2026",
-  ihsg1W: -1.52,
-  port1W: 0.33,
-
-  stocks: [
+  "owner": "Fajar",
+  "lastUpdate": "22 Apr 2026",
+  "ihsg1W": -1.52,
+  "port1W": 0.33,
+  "adminPin": "1234",
+  "stocks": [
     {
-      ticker: "BBRI",
-      lot: 7,
-      avgPrice: 3855,
-      currentPrice: 3270,
-      tp1: 3855,
-      tp2: 4200,
-      color: "#4cc9f0",
-      dividen: 206,
-      dividenDate: "8 Mei 2026"
+      "ticker": "BBRI",
+      "companyName": "Bank Rakyat Indonesia",
+      "lot": 7,
+      "avgPrice": 3855,
+      "currentPrice": 3270,
+      "tp1": 3855,
+      "tp2": 4200,
+      "color": "#4cc9f0",
+      "dividen": 206,
+      "dividenDate": "8 Mei 2026"
     },
     {
-      ticker: "BJTM",
-      lot: 4,
-      avgPrice: 570,
-      currentPrice: 595,
-      tp1: 650,
-      tp2: 750,
-      color: "#f72585",
-      dividen: 0,
-      dividenDate: ""
+      "ticker": "BJTM",
+      "companyName": "Bank Pembangunan Daerah Jawa Timur",
+      "lot": 4,
+      "avgPrice": 570,
+      "currentPrice": 595,
+      "tp1": 650,
+      "tp2": 750,
+      "color": "#f72585",
+      "dividen": 0,
+      "dividenDate": ""
     },
     {
-      ticker: "DRMA",
-      lot: 35,
-      avgPrice: 977,
-      currentPrice: 1040,
-      tp1: 1480,
-      tp2: 2148,
-      color: "#00e5a0",
-      dividen: 0,
-      dividenDate: ""
+      "ticker": "DRMA",
+      "companyName": "Dharma Polimetal",
+      "lot": 35,
+      "avgPrice": 977,
+      "currentPrice": 1040,
+      "tp1": 1480,
+      "tp2": 2148,
+      "color": "#00e5a0",
+      "dividen": 0,
+      "dividenDate": ""
     },
     {
-      ticker: "SSMS",
-      lot: 11,
-      avgPrice: 1352,
-      currentPrice: 1365,
-      tp1: 1480,
-      tp2: 2148,
-      color: "#ffd166",
-      dividen: 0,
-      dividenDate: ""
+      "ticker": "SSMS",
+      "companyName": "Sawit Sumbermas Sarana",
+      "lot": 11,
+      "avgPrice": 1352,
+      "currentPrice": 1365,
+      "tp1": 1480,
+      "tp2": 2148,
+      "color": "#ffd166",
+      "dividen": 0,
+      "dividenDate": ""
     }
   ]
 };
